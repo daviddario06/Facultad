bc0e80d91de662df2f99443c67df3cba2205ae92
David suyo <ddsdavid06@gmail.com>
2019-05-11 21:53:59 -0300

    prueba server

--- Marvel/src/Main.java
+++ Marvel/src/Main.java
@@ -14,16 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public Main() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+  
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
---
+++ Marvel/src/Prueba.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Connection conexion = null;
+		Statement miStatement = null;
+		ResultSet rs = null;
+		try {
+			
+			conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Marvel", "postgres", "postgre");
+		
+			miStatement = conexion.createStatement();
+			
+			rs = miStatement.executeQuery("SELECT * FROM PELICULAS");
+			
+			while (rs.next()) {
+				
+				System.out.println(rs.getString("NOMBRE"));
+			}
+			miStatement.close();
+			conexion.close();
+			rs.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+	
+		}
+	}
+
+}
