d08c0355a37cfc774afffc6e293b5798534e6912
noew <noeadones18@gmail.com>
2019-05-11 21:48:33 -0300

    segundo commit

---
+++ Marvel/src/Pelicula.java
@@ -0,0 +1,24 @@
+
+public class Pelicula {
+	
+	private String nombre;
+	private int emision;
+	private int cronologico;
+	private String estado;
+	
+	public int getEmision() {
+		return emision;
+	}
+	
+	public int getCronologico() {
+		return cronologico;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+}
